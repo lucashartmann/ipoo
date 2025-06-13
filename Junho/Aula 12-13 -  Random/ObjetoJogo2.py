a9aacc72039b8551b3d2a15b16548077aac73aa6
lucashartmann <lucas.a111@hotmail.com>
2025-06-13 11:55:13 -0300

    Aula 13.06

--- Junho/Aula 12-13 -  Random/ObjetoJogo2.py
+++ Junho/Aula 12-13 -  Random/ObjetoJogo2.py
@@ -4,11 +4,8 @@ objeto = ["Rocha", "Espada", "Capa", "Foice", "Capacete", "Peitoral", "Calça", 
 adjetivo = ["Feroz", "Fulminante", "Vingativa", "Bela como a Lua", "Reluzente", "Fervescente"]
 complemento = ["Forjada na lua", "do Minotauro", "dos confins do inferno", "Enferrujada", "Perfeita", "Usada pelo Rei de Minas"]
 
-def gerar_palavra(lista):
-    return random.choice(lista)
-
 def gerar_nome():
-    nome = f"{gerar_palavra(objeto)} {gerar_palavra(adjetivo)} {gerar_palavra(complemento)}" 
+    nome = f"{random.choice(objeto)} {random.choice(adjetivo)} {random.choice(complemento)}" 
     return nome
 
 item1 = gerar_nome()
